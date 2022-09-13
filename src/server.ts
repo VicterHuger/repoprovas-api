8354bec6d0de9e789903390da23e855e370d7dd8
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-09-13 12:56:59 -0300

    chore: add and configure settings to sing up and sign in users

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express, {json} from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import 'express-async-errors';
+import { errorHandlerMiddleware } from './middlewares/errorHandlerMiddleware';
+import router from './routes/router';
 
 dotenv.config();
 
 const app:express.Express = express();
 
-app.use([cors(), json]);
+app.use([cors(), json(), router, errorHandlerMiddleware]);
 
 const PORT:number = +process.env.PORT || 5001; 
 
