68109090b6a14a9f20e95f79d25b9158e4cdc47c
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-09-14 18:14:55 -0300

    test: better tests of login cicle with faker dependecy to generate randoms values

--- tests/signIn.test.ts
+++ tests/signIn.test.ts
@@ -3,6 +3,11 @@ import app from "../src/app";
 import prisma from "../src/config/databse";
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import { faker } from '@faker-js/faker';
+
+// const email = faker.internet.email();
+// const password = faker.internet.password();
+//faker.mersenne.rand();
 
 dotenv.config();
 
@@ -17,54 +22,57 @@ describe('POST /sign-in',()=>{
     });
 
     it('should return 422 if it is not given an email body property' ,async()=>{
-        const result = await supertest(app).post('/sign-in').send({password:'123'});
+        const result = await supertest(app).post('/sign-in').send({password:faker.internet.password()});
         expect(result.status).toBe(422);
     });
 
     it('should return 422 if it is not given a password body property' ,async()=>{
-        const result = await supertest(app).post('/sign-in').send({email:'victor@email.com'});
+        const result = await supertest(app).post('/sign-in').send({email:faker.internet.email()});
         expect(result.status).toBe(422);
     });
 
     it('should return 422 if an invalid email is supplyied and all others body properties are correct', async()=>{
-        const result = await supertest(app).post('/sign-in').send({email:'123'});
+        const result = await supertest(app).post('/sign-in').send({email:faker.random.alphaNumeric(), password: faker.internet.password()});
         expect(result.status).toBe(422);
     });
 
     it('should return 422 if a password with length less than 1 is supplyied and all others body properties are correct', async()=>{
-        const result = await supertest(app).post('/sign-in').send({email:'victor@email.com', password:'123'});
+        const result = await supertest(app).post('/sign-in').send({email:faker.internet.email(), password:''});
         expect(result.status).toBe(422);
     });
 
     it('should return 422 if the email supplyied is not string type and all others body properties are correct', async()=>{
-        const result = await supertest(app).post('/sign-up').send({email:123, 
-        password:'123456'});
+        const result = await supertest(app).post('/sign-up').send({email:faker.mersenne.rand(), 
+        password:faker.internet.password()});
         expect(result.status).toBe(422);
     });
     it('should return 422 if the password supplyied is not string type and all others body properties are correct', async()=>{
-        const result = await supertest(app).post('/sign-up').send({email:'teste@gmail.com', 
-        password:123456});
+        const result = await supertest(app).post('/sign-up').send({email:faker.internet.email(), 
+        password:faker.mersenne.rand()});
         expect(result.status).toBe(422);
     });
 
     it('should return with status 401 if the email passed was not sign-up and all body properties are correct', async()=>{
-        const result = await supertest(app).post('/sign-in').send({email:'teste@gmail.com', password:'123456'});
+        const result = await supertest(app).post('/sign-in').send({email:faker.internet.email(), password:faker.internet.password()});
         expect(result.status).toBe(401);
     });
 
     it('should return with status 401 if the password passed is not correct for an email sign-up and all body properties are correct', async()=>{
-        await supertest(app).post('/sign-up').send({email:'teste@gmail.com', password:'123456', confirmPassword:'123456'});
-        const result = await supertest(app).post('/sign-in').send({email:'teste@gmail.com', password:'123457'});
+        const email = faker.internet.email();
+        const password = faker.internet.password();
+        await supertest(app).post('/sign-up').send({email, password, confirmPassword:password});
+        const result = await supertest(app).post('/sign-in').send({email, password:faker.internet.password()});
         expect(result.status).toBe(401);
     });
 
     it('should return with status 200 and a valid token for the user, if the all the required body properties are correct and the user has previously sign-up', async()=>{
-        
-        await supertest(app).post('/sign-up').send({email:'teste@gmail.com', password:'123456', confirmPassword:'123456'});
+        const email = faker.internet.email();
+        const password = faker.internet.password();
+        await supertest(app).post('/sign-up').send({email, password, confirmPassword:password});
 
-        const user = await prisma.user.findUnique({where:{email:'teste@gmail.com'}});
+        const user = await prisma.user.findUnique({where:{email}});
         
-        const result = await supertest(app).post('/sign-in').send({email:'teste@gmail.com', password:'123456'});
+        const result = await supertest(app).post('/sign-in').send({email, password});
 
         const {userId}=jwt.verify(<string>result.body.token, process.env.TOKEN_SECRET_KEY) as {userId:number}
 
--- tests/signUp.test.ts
+++ tests/signUp.test.ts
@@ -1,6 +1,7 @@
 import supertest from "supertest";
 import app from "../src/app";
 import prisma from "../src/config/databse";
+import { faker } from '@faker-js/faker';
 
 beforeEach(async()=>{
     await prisma.$executeRaw`TRUNCATE TABLE users RESTART IDENTITY`;
@@ -12,57 +13,77 @@ describe('POST /sign-up', ()=>{
         expect(result.status).toBe(422); 
     });
     it('should return 422 if it is not given an email body property', async()=>{
-        const result = await supertest(app).post('/sign-up').send({password:'123456', confirmPassword:'123456'});
+        const password = faker.internet.password();
+        const result = await supertest(app).post('/sign-up').send({password, confirmPassword:password});
         expect(result.status).toBe(422);
     });
     it('should return 422 if it is not given a password body property', async()=>{
-        const result = await supertest(app).post('/sign-up').send({email:'victor@gmail.com', confirmPassword:'123456'});
+        const result = await supertest(app).post('/sign-up').send({email:faker.internet.email(), confirmPassword:faker.internet.password()});
         expect(result.status).toBe(422);
     });
     it('should return 422 if it is not given a confirm password body property', async()=>{
-        const result = await supertest(app).post('/sign-up').send({email:'victor@gmail.com', password:'123456'});
+        const result = await supertest(app).post('/sign-up').send({email:faker.internet.email(), password:faker.internet.password});
         expect(result.status).toBe(422);
     });
     it('should return 422 if confirm password and password do not match', async()=>{
-        const result = await supertest(app).post('/sign-up').send({email:'victor@gmail.com', 
-        password:'123456', confirmPassword:'123451'});
+        const result = await supertest(app).post('/sign-up').send({email:faker.internet.email(), 
+        password:faker.internet.password(), confirmPassword:faker.internet.password()});
         expect(result.status).toBe(422);
     });
     it('should return 422 if an invalid email is supplyied and all others body properties are correct', async()=>{
-        const result = await supertest(app).post('/sign-up').send({email:'123anything', 
-        password:'123456', confirmPassword:'123456'});
+        const password = faker.internet.password();
+        const result = await supertest(app).post('/sign-up').send({email:faker.random.alphaNumeric(), 
+        password, confirmPassword:password});
         expect(result.status).toBe(422);
     });
     it('should return 422 if a password with length less than 6 is supplyied and all others body properties are correct', async()=>{
-        const result = await supertest(app).post('/sign-up').send({email:'teste@gmail.com', 
-        password:'12345', confirmPassword:'12345'});
+        const password = faker.internet.password(5);
+        const result = await supertest(app).post('/sign-up').send({email:faker.internet.email(), 
+        password:password, confirmPassword:password});
         expect(result.status).toBe(422);
     });
     it('should return 422 if the email supplyied is not string type and all others body properties are correct', async()=>{
-        const result = await supertest(app).post('/sign-up').send({email:123, 
-        password:'123456', confirmPassword:'123456'});
+        const password = faker.internet.password();
+        const result = await supertest(app).post('/sign-up').send({email:faker.mersenne.rand(), 
+        password, confirmPassword:password});
         expect(result.status).toBe(422);
     });
     it('should return 422 if the password supplyied is not string type and all others body properties are correct', async()=>{
-        const result = await supertest(app).post('/sign-up').send({email:'teste@gmail.com', 
-        password:123456, confirmPassword:'123456'});
+        const password = faker.mersenne.rand();
+        const result = await supertest(app).post('/sign-up').send({
+            email:faker.internet.email(), 
+            password,
+            confirmPassword:password.toString()
+        });
         expect(result.status).toBe(422);
     });
     it('should return 422 if the confirmPassword supplyied is not string type and all others body properties are correct', async()=>{
-        const result = await supertest(app).post('/sign-up').send({email:'teste@gmail.com', 
-        password:'123456', confirmPassword:123456});
+        const result = await supertest(app).post('/sign-up').send({email:faker.internet.email(), 
+        password: faker.internet.password()
+        , confirmPassword:faker.mersenne.rand()});
         expect(result.status).toBe(422);
     });
 
     it('should return 201 and should sign up user on database if a correct body is supplyied', async()=>{
-        const result = await supertest(app).post('/sign-up').send({email:'victor@gmail.com', password:'123456', confirmPassword:'123456'});
-        const user = await prisma.user.findUnique({where:{email:'victor@gmail.com'}})
+        const email = faker.internet.email();
+        const password = faker.internet.password();
+        const result = await supertest(app).post('/sign-up').send({
+            email, 
+            password, confirmPassword:password
+        });
+        const user = await prisma.user.findUnique({where:{email}})
         expect(result.status).toBe(201);
         expect(user).not.toBe(null);
     });
     it('should return 409 if a correct body is supplyied and the email passed has already been sign up', async()=>{
-        await supertest(app).post('/sign-up').send({email:'victor@gmail.com', password:'123456', confirmPassword:'123456'});
-        const result = await supertest(app).post('/sign-up').send({email:'victor@gmail.com', password:'123456', confirmPassword:'123456'});
+        const email = faker.internet.email();
+        const password = faker.internet.password();
+        await supertest(app).post('/sign-up').send({
+            email,
+            password,
+            confirmPassword:password
+        });
+        const result = await supertest(app).post('/sign-up').send({email:email, password, confirmPassword:password});
         expect(result.status).toBe(409);
     });
 })
