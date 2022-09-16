8d0ebca5fe4eabf12c17f8c50168f931a82855cc
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-09-16 12:43:35 -0300

    refactor: testController send the test created when it was sucessfull created

--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -4,6 +4,6 @@ import { TypeTestSchema } from "../types/testType";
 
 export async function createTest(_req:Request, res:Response){
     const body:TypeTestSchema = res.locals.body;
-    await testService.createTest(body)
-    return res.sendStatus(201);
+    const test = await testService.createTest(body)
+    return res.status(201).send(test);
 }
